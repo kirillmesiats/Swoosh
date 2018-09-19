0210935f3745e9bfcd1e7f68f2bd64ede6136b68
kirillmesiats <a55a4@yandex.ru>
2018-09-19 15:50:17 +0300

    skill activity layout

--- app/src/main/java/eu/relsys/swoosh/LeagueActivity.kt
+++ app/src/main/java/eu/relsys/swoosh/LeagueActivity.kt
@@ -1,12 +1,18 @@
 package eu.relsys.swoosh
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
